d3ba7bc3814f1668416801930f88921ad13ed66c
Yin Yibo <yinyb811@gmail.com>
2023-03-18 23:46:52 +0800

    first commit

---
+++ hw2.c1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinkList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            this.Next = null;
+            this.Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> tail;
+        private Node<T> head;
+        public GenericList() 
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t) 
+        {
+            Node<T> n =new Node<T>(t);
+            if (tail == null) 
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action) 
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+            Console.Write("Elements in list is: ");
+            intlist.ForEach(s => Console.Write(s + " "));
+            Console.WriteLine();
+
+            Console.Write("Maximum of list is: ");
+            int max = intlist.Head.Data;
+            intlist.ForEach(s => { if (s >= max) max = s; });
+            Console.WriteLine(max);
+
+            Console.Write("Minimum of list is: ");
+            int min = intlist.Head.Data;
+            intlist.ForEach(s => { if (s <= min) min = s; });
+            Console.WriteLine(min);
+
+            Console.Write("Summary of list is: ");
+            int sum = 0;
+            intlist.ForEach(s=>sum += s);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ hw2.c2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClockspace
+{
+    public delegate void TickHandler(object sender, TickEventArgs e);
+    public delegate void AlarmHandler(object sender, AlarmEventArgs e);
+    public class TickEventArgs{ }  
+    public class AlarmEventArgs{ }
+    public class AlarmClock
+    {
+        public event TickHandler OnTick;
+        public event AlarmHandler AlarmTriggers;
+        public void Tick1()
+        {
+            Console.WriteLine("Alarm Clock Starts! ");
+            Console.WriteLine("It is ticking....");
+            TickEventArgs args = new TickEventArgs();
+            OnTick(this, args);
+        }
+        public void Alarm1() 
+        {
+            AlarmEventArgs args = new AlarmEventArgs();
+            AlarmTriggers(this, args);
+        }
+    }
+    public class Form
+    {
+        public AlarmClock clock1= new AlarmClock();
+        public Form() 
+        {
+            clock1.OnTick += new TickHandler(alarm_OnTick);
+            clock1.AlarmTriggers += new AlarmHandler(alarm_Alarm);
+        }
+        void alarm_OnTick(object sender, TickEventArgs e)
+        {
+            Console.Write("Time now is: ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ticking ends up.");
+        }
+        void alarm_Alarm(object sender, AlarmEventArgs e)
+        {
+            Thread.Sleep(4500);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.Write("  <Alarm Clock Rings! ! !>  \n");
+            Console.Write("And numbers turn blue.... ");
+        }
+    }
+    class Mainclass
+    {
+        static void Main(string[] args)
+        {
+            Form f = new Form();
+            Parallel.Invoke(
+                ()=>f.clock1.Tick1(),
+                ()=>f.clock1.Alarm1()
+            );
+        }
+    }
+}
